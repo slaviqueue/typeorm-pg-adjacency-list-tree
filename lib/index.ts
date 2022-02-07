2d5f9921de2f92149dc47aed93511f329f949369
Viacheslav Shvets <viacheslav_shvets@external.mckinsey.com>
2022-02-07 14:05:01 +0200

    fixed exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,42 +1,5 @@
 import 'reflect-metadata'
-import { Column, createConnection, Entity, EntityRepository, getConnection, PrimaryGeneratedColumn } from 'typeorm'
-import { Tree } from './decorator/tree'
-import { TreeChildren } from './decorator/tree-children'
-import { TreeParent } from './decorator/tree-parent'
-import { TreeRepository } from './repository/tree-repository'
-
-@Tree()
-@Entity()
-class Node {
-  @PrimaryGeneratedColumn()
-  public id!: number
-
-  @Column()
-  public value!: number
-
-  @TreeParent()
-  public parent!: Node
-
-  @TreeChildren()
-  public children!: Node[]
-}
-
-@EntityRepository(Node)
-class NodeRepo extends TreeRepository<Node> {}
-
-;(async () => {
-  await createConnection({
-    type: 'postgres',
-    username: 'postgres',
-    password: 'postgres',
-    entities: [Node],
-    logging: true,
-  })
-
-  const repo = getConnection().getCustomRepository(NodeRepo)
-
-  // await repo.save({ value: 1, children: [{ value: 2 }] })
-
-  // console.log(await repo.findDescendants(9))
-  console.log(await repo.findTrees())
-})()
+export { Tree } from './decorator/tree'
+export { TreeChildren } from './decorator/tree-children'
+export { TreeParent } from './decorator/tree-parent'
+export { TreeRepository } from './repository/tree-repository'
